0ee819fbee2a79a5950a1c1253bd88f4ba56ed47
= <abdullahijimoh3.ja@gmail.com>
2024-11-30 15:48:55 +0100

    Add a date spm package

---
+++ GoPaddi/CreateTrip/Controllers/GoPPadi-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  GoPPadi-Bridging-Header.h
+//  GoPaddi
+//
+//  Created by Abdullah on 29/11/2024.
+//
+
+#ifndef GoPPadi_Bridging_Header_h
+#define GoPPadi_Bridging_Header_h
+
+
+#endif /* GoPPadi_Bridging_Header_h */
--- GoPaddi/CreateTrip/Controllers/PickDateController.swift
+++ GoPaddi/CreateTrip/Controllers/PickDateController.swift
@@ -6,13 +6,24 @@
 //
 
 import UIKit
+import FSCalendar
 
 final class PickDateController: MainBaseController {
 
+    var calendar: FSCalendar?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar(title: "Date")
         view.backgroundColor = .white
+        
+        // In loadView or viewDidLoad
+//        let calendar = FSCalendar(frame: CGRect(x: 0, y: 0, width: Constant.screenWidth, height: Constant.screenHeight))
+//        calendar.dataSource = self
+//        calendar.delegate = self
+//        view.addSubview(calendar)
+//        self.calendar = calendar
+        setupView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -22,4 +33,24 @@ final class PickDateController: MainBaseController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
     }
+    
+    func setupView() {
+        calendar = FSCalendar(frame: .zero)
+        view.addSubview(calendar!)
+        calendar!.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+        calendar?.dataSource = self
+        calendar?.delegate = self
+    }
+}
+
+extension PickDateController: FSCalendarDataSource {
+    
+}
+
+extension PickDateController: FSCalendarDelegate {
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        debugPrint(date)
+    }
+    
 }
